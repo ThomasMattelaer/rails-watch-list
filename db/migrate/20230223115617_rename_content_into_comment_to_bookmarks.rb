16f72663673e209fa23b0170555704c1af516359
Thomas Mattelaer mattelaer.thomas@hotmail.com <mattelaer.thomas@hotmail.com>
2023-02-24 16:39:43 +0100

    Bundle fix

---
+++ db/migrate/20230223115617_rename_content_into_comment_to_bookmarks.rb
@@ -0,0 +1,5 @@
+class RenameContentIntoCommentToBookmarks < ActiveRecord::Migration[7.0]
+  def change
+    rename_column :bookmarks, :content, :comment
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'JSON'
+require 'open-uri'
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+movies = JSON.parse(URI.open(url).read)
+
+movies["results"].each do |movie|
+  movie = Movie.create(
+    title: movie['original_title'],
+    overview: movie["overview"],
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}",
+    rating: movie['vote_average']
+  )
+  puts "Creating : #{movie.id} #{movie.title}"
+end
+
+puts "finished !"
